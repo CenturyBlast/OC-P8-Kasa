f2345067a6230689e2cbf0cc97ee00f251f99e50
Maxime Chambon <max.chambon@protonmail.ch>
2023-09-09 17:09:38 +0200

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dev de Kasa en cours 👌🛠️ 
         </p>
         <a
           className="App-link"

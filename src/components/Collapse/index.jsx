d01a6024f5c3ef296530d182e45edf7deb1f1496
Maxime Chambon <max.chambon@protonmail.ch>
2023-10-17 20:51:45 +0200

    Final responsive

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -7,22 +7,21 @@ export default function Collapse({ title, content }) {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <>
-            <div className="Collapse" >
-                <h3 className='Collapse__title' onClick={() => setToggle(!toggle)} >
-                    {title}
-                    <img
-                        className={toggle ? 'Arrow Arrow__up' : 'Arrow Arrow__down'}
-                        src={collArrow}
-                        alt="contenu visible"
-                    />
-                </h3>
-                <div className={toggle ? 'Collapse__content' : 'Collapse__content_hidden'}>
-                    {Array.isArray(content) ? content.map((item, index) => {
-                        return (<p key={index}>{item}</p>)
-                    }) : content}
-                </div>
+        <div className="Collapse" >
+            <h3 className='Collapse__title'>
+                {title}
+                <img
+                    onClick={() => setToggle(!toggle)}
+                    className={toggle ? 'Arrow Arrow__up' : 'Arrow Arrow__down'}
+                    src={collArrow}
+                    alt="contenu visible"
+                />
+            </h3>
+            <div className={toggle ? 'Collapse__content' : 'Collapse__content_hidden'}>
+                {Array.isArray(content) ? content.map((item, index) => {
+                    return (<div key={index}>{item}</div>)
+                }) : content}
             </div>
-        </>
+        </div>
     );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="Home">
       <div className="Home__banner">
         <img src={homeImg} alt="front de mer" className='Home__banner_img' />
-        <span className='Home__banner_text'>Chez vous, partout et ailleurs</span>
+        <span className='Home__banner_text'>Chez vous,<br/> partout et ailleurs</span>
       </div>
       <div className="Home__cardlist">
         {cardData.map((item) => (
